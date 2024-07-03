224d678126b989ea0d2e022dfd2040a0132a7a96
Tauan Camargo <lukyhenson@gmail.com>
2024-07-03 12:04:36 -0300

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Tauan Tathiell Camargo on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
